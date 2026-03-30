6ddba4fbd71d9fda61029cb87e9a67b638373d47
0XMatriX <113830943+matrixo-pog@users.noreply.github.com>
2026-03-30 11:15:08 +0200

    Routing v.2

--- poker/src/app/Components/create-lobby.jsx
+++ poker/src/app/Components/create-lobby.jsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { useState, useRef } from "react";
+import Link from "next/link";
 
 export default function CreateLobby() {
     const [plyrCount, setPlyrCount] = useState(4);
     const plyrRange = useRef();
 
     return (
-        <div className="max-w-full max-h-240 bg-gray-800">
+        <div className="animate-bg max-w-full max-h-240 bg-radial-[at_50%_50%] from-gray-900 via-gray-900 to-gray-950 to-90%">
             <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-red-900 from-30% to-amber-300 to-70% text-5xl font-black text-center p-5">Stwórz pokój</h1>
             <form id="create-form" className="text-center p-8 h-220">
                 <label htmlFor="plyrRange" className="text-white text-2xl font-black">Liczba graczy: {plyrCount}</label>
@@ -36,11 +37,13 @@ export default function CreateLobby() {
                             focus:ring-4 focus:outline-none focus:ring-amber-500 dark:focus:ring-amber-800" type="text" name="startValue"></input>
                         <label className="text-white text-xl font-black mt-16">Czas na ruch:</label>
                         <input className="w-120 bg-amber-50 p-2 mb-15 rounded-xl border-yellow-600 border-solid border-4
-                            focus:ring-4 focus:outline-none focus:ring-amber-500 dark:focus:ring-amber-800" type="text" name="moveTime"></input>
-                        <button type="button" onClick={() => window.location.href = '/'} className="bg-gradient-to-r from-green-400 via-green-500 to-green-600
-                            hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 
-                            shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 rounded-base text-center
-                            w-100 p-4 m-5 border-3 border-black self-start rounded-xl text-5xl font-black cursor-pointer">Wróć</button>
+                            focus:ring-4 focus:outline-none focus:ring-amber-500 dark:focus:ring-amber-800" type="number" name="moveTime"></input>
+                        <Link className="self-start" href="/">
+                            <button type="button" className="bg-gradient-to-r from-green-400 via-green-500 to-green-600
+                                hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 
+                                shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 rounded-base text-center
+                                w-100 p-4 m-5 border-3 border-black rounded-xl text-5xl font-black cursor-pointer">Wróć</button>
+                        </Link>
                     </div>
                 </div>
             </form>
