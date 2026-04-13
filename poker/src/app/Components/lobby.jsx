a19b308f9bbc22b21e55eb55b87853528249afa8
0XMatriX <113830943+matrixo-pog@users.noreply.github.com>
2026-04-13 09:11:34 +0200

    Cosmetic change

--- poker/src/app/Components/lobby.jsx
+++ poker/src/app/Components/lobby.jsx
@@ -1,41 +1,90 @@
-import LobbyPlayer from "./lobbyPlayer";
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import LobbyPlayer from "./elements/lobbyPlayer";
+import axios from "axios";
+import { io } from "socket.io-client";
 
-export default async function Lobby() {
+const socket = io("http://localhost:8080");
+
+export default function Lobby() {
+    const [players, setPlayers] = useState([]);
+    const [code, setCode] = useState("");
+    const [playerId, setPlayerId] = useState("");
+    const [isHost, setIsHost] = useState(false);
+    const router = useRouter();
+
+    const fetchPlayers = async () => {
+        if (!code) return;
+        try {
+            const res = await axios.post("http://localhost:8080/getListOfPlayers", {
+                code: code
+            });
+            setPlayers(res.data.players);
+        } catch (error) {
+            console.error("Error fetching players:", error);
+        }
+    };
+
+    const handleLeave = async () => {
+        socket.emit("leave_room", playerId);
+        sessionStorage.clear();
+        router.push("/");
+    }
+
+    useEffect(() => {
+        const storedCode = sessionStorage.getItem("code");
+        const storedPlayerId = sessionStorage.getItem("playerId");
+        const storedIsHost = sessionStorage.getItem("isHost");
+        setCode(storedCode);
+        setPlayerId(storedPlayerId);
+        setIsHost(storedIsHost);
+    }, []);
+
+    useEffect(() => {
+        if (!code) return;
+        if (!playerId) return;
+        socket.emit("auth", playerId);
+
+        socket.on("refresh_list", fetchPlayers);
+
+        return () => {
+            console.log("cleaning up");
+            socket.off("refresh_list", fetchPlayers);
+        };
+    }, [code, playerId]);
+
     return (
         <div className="max-w-full h-240 bg-gray-800">
             <div className="grid columns-3 grid-flow-col">
                 <div className=""></div>
                 <div className="">
                     <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-red-900 from-30% to-amber-300 to-70% text-5xl font-black text-center p-5">Pokój</h1>
-                    <h2 className="font-black text-center text-3xl text-white">Kod:</h2>
+                    <h2 className="font-black text-center text-3xl text-white">Kod: {code}</h2>
                     <div className="flex items-center max-w-full h-full max-h-9/10 gap-5.5 flex-col pt-5">
                         <div className="flex flex-col w-240 h-164 border-3 rounded-s-4xl border-white p-5 gap-5 overflow-y-auto overflow-x-hidden">
-                            <LobbyPlayer name="Gracz 1"/>
-                            <LobbyPlayer name="Gracz 2"/>
-                            <LobbyPlayer name="Gracz 3"/>
-                            <LobbyPlayer name="Gracz 4"/>
-                            <LobbyPlayer name="Gracz 5"/>
-                            <LobbyPlayer name="Gracz 6"/>
-                            <LobbyPlayer name="Gracz 7"/>
-                            <LobbyPlayer name="Gracz 8"/>
+                            {players?.map((player, index) => (
+                                <LobbyPlayer key={index} name={player.username} isItMe={player.id == playerId}/>
+                            ))}
                         </div>
                         <div>
-                            <button type="button" className="bg-gradient-to-r from-rose-500 via-red-700 to-red-800 
+                            <button type="button" onClick={handleLeave}
+                            className="text-gray-200 bg-gradient-to-r from-rose-500 via-red-700 to-red-800 
                                 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-500 dark:focus:ring-red-800 
                                 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 rounded-base text-center
                                 w-100 p-4 m-3 border-4 border-black self-start rounded-xl text-5xl font-black cursor-pointer">Wyjdź</button>
-                            <button type="button" className="text-gray-200 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 
-                                hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-700 dark:focus:ring-red-800 
-                                shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 rounded-base text-center
-                                w-100 p-4 m-3 border-3 border-red-800 self-start rounded-xl text-5xl font-black cursor-pointer">Start</button>
+                                
+                                {isHost == 'true' ? (
+                                    <button type="button" className="text-gray-200 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 
+                                        hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-700 dark:focus:ring-red-800 
+                                        shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 rounded-base text-center
+                                        w-100 p-4 m-3 border-3 border-red-800 self-start rounded-xl text-5xl font-black cursor-pointer">Start</button>
+                                ) : null}
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    {/* <h2 className="font-black text-white text-2xl m-5">Czas na ruch:</h2>
-                    <h2 className="font-black text-white text-2xl m-5">Small blind:</h2>
-                    <h2 className="font-black text-white text-2xl m-5">Poczatkowa stawka:</h2> */}
-                </div>
+                <div className="flex flex-col justify-center items-center"></div>
             </div>
         </div>
     );
