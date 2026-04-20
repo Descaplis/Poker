fc88a7eca3a1d962a88c3e2412465c01908595e6
0XMatriX <113830943+matrixo-pog@users.noreply.github.com>
2026-04-20 11:18:51 +0200

    Players interactions
    Added fold all in and blinds styles.

--- poker/src/app/Components/theGame.jsx
+++ poker/src/app/Components/theGame.jsx
@@ -10,22 +10,22 @@ return (
     <div className="relative w-10/11 flex items-center justify-center">
       {/* Gracze po lewej (8 i 7) */}
       <div className="flex flex-col justify-around gap-[10vh] h-[60vh] md:h-[50vh]">
-        <Player name="Gracz 8" position="left"/>
-        <Player name="Gracz 7" position="left"/>
+        <Player name="Gracz 8" position="left" balance={0} isAllIn={true}/>
+        <Player name="Gracz 7" position="left" balance={0} isAllIn={false}/>
       </div>
 
         {/* Środek: Góra, Stół, Dół */}
         <div className="flex-1 flex flex-col items-center">
             {/* Gracze na górze (1 i 2) */}
             <div className="flex justify-evenly w-full max-w-2xl gap-4">
-              <Player name="Gracz 1"/>
-              <Player name="Gracz 2" />
+              <Player name="Gracz 1" balance={100} isAllIn={false}/>
+              <Player name="Gracz 2" blind="small"/>
             </div>
 
             {/* STÓŁ */}
             <div className="w-full aspect-2/1 grow flex items-center justify-center p-2 md:p-4 mt-[4vh]">
-                <div className="aspect-2/1 w-full border-amber-900 border-4 lg:border-11 bg-radial-[at_35%_35%] from-gray-500 to-black rounded-[50px] lg:rounded-[80px] p-3 lg:p-6 shadow-2xl">
-                    <div className="flex justify-center items-center w-full h-full bg-radial-[at_35%_35%] from-green-600 to-green-800 rounded-[40px] gap-3">
+                <div className="aspect-2/1 w-full border-amber-900 border-4 lg:border-6 bg-radial-[at_35%_35%] from-gray-500 to-black rounded-[50px] lg:rounded-[80px] p-3 lg:p-6 shadow-2xl">
+                    <div className="flex justify-center items-center w-full h-full bg-radial-[at_35%_35%] from-green-600 to-green-950 rounded-[40px] gap-3">
                         <div className="w-[5vw] h-[15vh]">
                             <img src="/images/karty/BackCard.png"></img>
                         </div>
@@ -47,14 +47,14 @@ return (
 
             {/* Gracze na dole (5 i 6) */}
             <div className="flex justify-evenly w-full max-w-2xl gap-4">
-                <Player name="Gracz 6" position="down" cards={["3Pik", "queenKier"]}/>
-                <Player name="Gracz 5" position="down"/>
+                <Player name="Gracz 6" position="down" cards={["3Pik", "queenKier"]} balance={10} isAllIn={false}/>
+                <Player name="Gracz 5" position="down" balance={48} isFolded={true}/>
             </div>
       </div>
 
       {/* Gracze po prawej (3 i 4) */}
       <div className="flex flex-col justify-around gap-[10vh] h-[60vh] md:h-[50vh]">
-        <Player name="Gracz 3" position="right"/>
+        <Player name="Gracz 3" position="right" blind="big"/>
         <Player name="Gracz 4" position="right"/>
       </div>
     </div>
