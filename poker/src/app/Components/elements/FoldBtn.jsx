4c5ce1559396320a427cea768b90d1d2bdcd9211
0XMatriX <113830943+matrixo-pog@users.noreply.github.com>
2026-04-23 11:14:08 +0200

    Buttons, main and side pulls

---
+++ poker/src/app/Components/elements/FoldBtn.jsx
@@ -0,0 +1,12 @@
+export default function FoldBtn({show}){
+    if(!show) return;
+    return (
+        <div className="">
+            <button className="w-[10vw] h-[8vh] bg-radial-[at_20%_25%] from-red-700 via-red-600 via-50% to-rose-600 to-90% rounded-2xl cursor-pointer
+            border-2 border-amber-900">
+                <p className="text-black text-3xl font-black">Pass</p>
+            </button>
+        </div>
+
+    );
+}
--- poker/src/app/Components/elements/RaiseBtn.jsx
+++ poker/src/app/Components/elements/RaiseBtn.jsx
@@ -1,14 +1,43 @@
-export default function RaiseBtn(){
+'use client'
+import { useRef, useEffect } from "react";
+
+export default function RaiseBtn({show}){
+    if(!show) return;
+
+    const modal = useRef();
+
     const popUp = () => {
-        var czyWysw = false;
+        modal.current.style.display = "block";
     }
 
+    const closePop = () => {
+        modal.current.style.display = "none";
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (modal.current && event.target === modal.current) {
+                closePop();
+            }
+        }
+        window.addEventListener('click', handleClickOutside);
+        return () => window.removeEventListener('click', handleClickOutside);
+    }, []);
+
     return (
-        <div className="h-200 flex justify-center items-end">
-            <button className="w-1/9 h-[8vh] bg-radial-[at_20%_25%] from-amber-500 via-orange-400 via-45% to-yellow-300 to-90% rounded-2xl cursor-pointer"
-                onClick={popUp()}>
-                <p className="bg-clip-text text-transparent bg-linear-to-r from-red-800 from-30% to-amber-950 to-70% text-3xl font-black">Podbij</p>
+        <div className="">
+            <button className="w-[10vw] h-[8vh] bg-radial-[at_20%_25%] from-amber-500 via-orange-400 via-45% to-yellow-300 to-90% rounded-2xl cursor-pointer
+            border-2 border-amber-900" onClick={popUp}>
+                <p className="bg-clip-text text-transparent bg-linear-to-r from-red-800 to-black text-3xl font-black">Podbij</p>
             </button>
+            <div className="fixed z-1 left-0 top-0 w-screen h-screen bg-black/40" ref={modal} style={{display: 'none'}}>
+                <div className="relative flex flex-col bg-radial-[at_20%_25%] from-amber-500 via-orange-400 via-45% to-yellow-300 to-90%
+                    m-auto p-[1%] border border-black w-[20vw] shadow-xl/30 rounded-b-2xl animate-popup">
+                    <label className="font-black text-lg">O ile chcesz podbić:</label>
+                    <input type="number" className="bg-amber-50 mt-[1vh] p-[1%] rounded-xl" min={0}/>
+                    <button className="mx-auto mt-4 bg-amber-800 p-2 w-[10vw] text-white font-bold rounded-xl cursor-pointer">Potwierdź</button>
+                </div>
+            </div>
         </div>
 
     );
