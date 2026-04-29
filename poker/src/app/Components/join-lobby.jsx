8f3e03ec926d7ab5d7bdbda7b96d3a7a37f25546
0XMatriX <113830943+matrixo-pog@users.noreply.github.com>
2026-04-29 17:26:11 +0200

    The REAL Nothing Update v.4

--- poker/src/app/Components/join-lobby.jsx
+++ poker/src/app/Components/join-lobby.jsx
@@ -62,13 +62,15 @@ export default function JoinLobby(){
                 ))}
             </div>
 
-            <div className="animate-bg max-w-full h-239 bg-radial-[at_50%_55%] from-gray-950 via-gray-950 to-gray-900 to-90%">
+            <div className="animate-bg max-w-full h-screen bg-radial-[at_50%_55%] from-gray-950 via-gray-950 to-gray-900 to-90%">
                 <h1 className="bg-clip-text text-transparent bg-linear-to-r from-red-900 from-30% to-amber-300 to-70% text-5xl font-black text-center p-5">Dołącz do lobby</h1>
                 <form id="join-form" className="p-5 flex flex-col items-center">
                     <label className="text-gray-200 font-black text-2xl p-2">Nazwa gracza:</label>
-                    <input type="text" className="w-100 bg-amber-50 p-2 rounded-xl text-black" name="nickname" onChange={(e) => setUsername(e.currentTarget.value.trim())}/>
+                    <input type="text" className="w-100 bg-amber-50 p-2 rounded-xl text-black focus:ring-4 focus:shadow-2xl focus:shadow-amber-500 dark:focus:ring-amber-800"
+                        name="nickname" onChange={(e) => setUsername(e.currentTarget.value.trim())}/>
                     <label className="text-gray-200 font-black text-2xl p-2 mt-10">Kod pokoju:</label>
-                    <input type="text" className="w-100 bg-amber-50 p-2 rounded-xl text-black" name="roomCode" onChange={(e) => setCode(e.currentTarget.value.trim())}/>
+                    <input type="text" className="w-100 bg-amber-50 p-2 rounded-xl text-black focus:ring-4 focus:shadow-2xl focus:shadow-amber-500 dark:focus:ring-amber-800"
+                        name="roomCode" onChange={(e) => setCode(e.currentTarget.value.trim())}/>
                     <div className="flex flex-row mt-20">
                         <button type="button" onClick={joinRoom} className="bg-linear-to-r from-amber-700 via-amber-500 to-amber-600
                             hover:bg-linear-to-br focus:ring-4 focus:shadow-2xl focus:shadow-amber-300 dark:focus:ring-amber-700
