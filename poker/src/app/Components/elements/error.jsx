25f7bdadca9eb7c28de6c2d2ded17ee9a40551a1
Kajetan Kraska <kajtas7424@gmail.com>
2026-04-29 21:23:25 +0200

    literally nothing update

--- poker/src/app/Components/elements/error.jsx
+++ poker/src/app/Components/elements/error.jsx
@@ -14,9 +14,9 @@ export default function Error({ text, onRemove }) {
     }, []);
 
     return (
-        <div class={`backdrop-blur-md bg-white/30 border border-white/20 p-6 rounded-2xl shadow-2xl flex flex-col items-center max-w-sm text-center
+        <div class={`backdrop-blur-md bg-red-500/50 border border-white/20 p-6 rounded-2xl shadow-2xl flex flex-col items-center max-w-sm text-center
             transition-all duration-1000 ease-in-out z-50 ${isLeaving ? 'opacity-0' : 'opacity-100'}`}>
-            <h1 className="text-red-500 text-4xl font-bold mb-2 text-shadow-lg text-shadow-red-300/50">No i co ty robisz?</h1>
+            <h1 className="text-red-600 text-4xl font-bold mb-2 text-shadow-lg text-shadow-red-300/50">No i co ty robisz?</h1>
             <p className="text-black font-bold text-sm mt-2">{text}</p>
         </div>
     );
